e6b8256abe959ea4e6a3a8dd6182ca23c3487126
Lukas Eder <eder.lukas@bluewin.ch>
2021-01-23 13:04:09 +0100

    Implemented highscore to snake safing it into a file.

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -13,18 +13,22 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-        self.high_score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.update_display()
 
     def increase_score(self):
         self.score += 1
 
     def update_display(self):
-        self.undo()
+        self.clear()
         self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset_scoreboard(self):
         if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.score}")
+
             self.high_score = self.score
         self.score = 0
         self.update_display()
