7557fea6284798273854b2aa55ebed975b9b33d9
Lukas Eder <eder.lukas@bluewin.ch>
2021-01-30 19:39:53 +0100

    Implemented error handling for project.

--- nato_phonetic_alphabet/main.py
+++ nato_phonetic_alphabet/main.py
@@ -3,6 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-name = input("What's the name you want to spell? ").upper()
-print([phonetic_dict[letter] for letter in name])
 
+def get_name():
+    name = input("What's the name you want to spell? ").upper()
+    try:
+        print([phonetic_dict[letter] for letter in name])
+    except KeyError:
+        print("Your input contained a letter which was not an asci")
+        get_name()
+
+
+get_name()
